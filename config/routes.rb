6954b9d814716decbbc90685fa170a6be1ba1a4d
Roman Truschev <truschev@gmail.com>
2012-08-13 21:34:22 +0400

    create tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Geopet::Application.routes.draw do
   devise_for :users
-  resources :users
+
+  resources :users  
+
+  namespace :api do
+    namespace :v1  do
+      resources :tokens, :only => [:create, :destroy]
+    end
+  end  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +58,7 @@ Geopet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
