c6fe5277d0abb5304dd0076587131efb2d754878
Roman Truschev <truschev@gmail.com>
2012-08-13 20:43:53 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geopet::Application.config.secret_token = '8f6d3277df824f02b6f8285efab9aa162b7a17ddd47a42f12416826e641e980395f0d97cd3868bab536ef08ceba53e831a2b2e064efe428c29de0a6a90621f7e'
